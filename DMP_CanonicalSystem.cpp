90f4ef57a978a2b54ba990c9610d3333983b7913
Somdeb <somdeb.electrical95@gmail.com>
2023-05-15 22:39:16 +0530

    initial commit

---
+++ DMP_CanonicalSystem.cpp
@@ -0,0 +1,68 @@
+
+
+#include<iostream>
+#include<cmath>
+#include<vector>
+
+
+using namespace std;
+
+
+class CanonicalSystem{
+
+    public:
+
+        double step_size;
+        double alpha_s;
+        double run_time;
+
+        double system_state;
+
+        CanonicalSystem(double step_size, double alpha_s, double run_time){
+
+            this-> step_size = step_size;
+            this-> run_time = run_time;
+            this-> alpha_s = alpha_s;
+
+        }
+
+        void reset(){
+            /*
+            Reset the system state
+            */
+            this->system_state = 1.0;
+        }
+
+        std::vector<double> rollout(double tau=1.0){
+
+            int timesteps = int(run_time/step_size);
+            std::vector<double> system_track;
+            reset();
+
+            for (int i=0; i<timesteps; i++);{
+
+                system_track.push_back(this->system_state);
+                step(tau);
+
+            }
+
+            return system_track;
+
+        }
+
+        double step(double tau=1.0, double error_coupling=1.0){
+
+            double cnst =  -1.0 * ((this->alpha_s / tau) / error_coupling);
+            this->system_state = this->system_state * exp(cnst*this->step_size);
+            return this->system_state;
+
+
+
+
+        }
+
+};
+
+int main(){
+    return 0;
+}
